5f698422831b8c4987a5648808319aa5cfed207c
tacevedooo <tacevedo@unal.edu.co>
2026-05-29 00:47:37 -0500

    fix2

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="SmartRouteApp",
-    api_url="https://TU-DOMINIO.up.railway.app",
+    api_url="smartrouteapp-production.up.railway.app",
+    deploy_url="smartrouteapp-production.up.railway.app",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
